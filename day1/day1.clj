31989c1af073a72d059ace05834f512de6099ce8
volesen <vincolesen@gmail.com>
2022-12-01 19:00:59 +0100

    Initial commit

---
+++ day1/day1.clj
@@ -0,0 +1,31 @@
+(require '[clojure.string :as str])
+
+(def input (slurp "day1/input.txt"))
+
+; Part 1
+
+(defn parse-calories [block]
+  (map #(Integer/parseInt %) (str/split-lines block)))
+
+(defn parse-input [input]
+  (map parse-calories (str/split input  #"\n\n")))
+
+(defn take-largest [n items]
+  (take-last n (sort items)))
+
+(defn sum [calories]
+  (reduce + calories))
+
+
+(->> input
+     (parse-input)
+     (map sum)
+     (take-largest 1))
+
+; Part 2
+
+(->> input
+     (parse-input)
+     (map sum)
+     (take-largest 3)
+     (sum))
