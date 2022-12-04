33315f28ebeeaa57bdca153c27110fb10f375bda
volesen <vincolesen@gmail.com>
2022-12-04 09:48:20 +0100

    Solved day 4

---
+++ day4/day4.clj
@@ -0,0 +1,40 @@
+(require '[clojure.string :as str])
+
+(def input (slurp "day4/input"))
+
+
+(defn parse-range
+  "Parse a string of the form '123-456' into a range of integers."
+  [s]
+  (let [[a b] (str/split s #"-")]
+    [(Integer/parseInt a) (Integer/parseInt b)]))
+
+(defn parse-pair [pair]
+  (map parse-range (str/split pair #",")))
+
+
+; Logic
+
+(defn fully-contains? [[[start1 end1] [start2 end2]]]
+  (or  (and (<= start1 start2) (<= end2 end1))
+       (and (<= start2 start1) (<= end1 end2))))
+
+
+(defn overlaps? [[[start1 end1] [start2 end2]]]
+  (or (<= start1 start2 end1)
+      (<= start2 start1 end2)))
+
+; Part 1
+(->> input
+     str/split-lines
+     (map parse-pair)
+     (filter fully-contains?)
+     count)
+
+
+; Part 2
+(->> input
+     str/split-lines
+     (map parse-pair)
+     (filter overlaps?)
+     count)
