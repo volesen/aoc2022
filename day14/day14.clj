f7dcee35d83c4577bbe3563b08d2767c19437373
volesen <vincolesen@gmail.com>
2022-12-14 18:25:33 +0100

    Clean up

--- day14/day14.clj
+++ day14/day14.clj
@@ -14,55 +14,55 @@
         max (max x1 x2)]
     (range min (inc max))))
 
-(defn expand [points]
+(defn expand-line [points]
   (for [[[x1 y1] [x2 y2]] (partition 2 1 points)
         x (between x1 x2)
         y (between y1 y2)]
     [x y]))
 
-(defn simulate-particle [bottom grid]
+(def rocks
+  (->> parsed
+       (mapcat expand-line)
+       set))
+
+(defn simulate-particle [bottom settled]
   (loop [[x y] [500 0]]
     (cond
       (>= y bottom) [x y]
 
       ; Go down
-      (not (contains? (set grid) [x (inc y)]))
+      (not (contains? (set settled) [x (inc y)]))
       (recur [x (inc y)])
 
       ; Go left
-      (not (contains? (set grid) [(dec x) (inc y)]))
+      (not (contains? (set settled) [(dec x) (inc y)]))
       (recur [(dec x) (inc y)])
 
       ; Go right
-      (not (contains? (set grid) [(inc x) (inc y)]))
+      (not (contains? (set settled) [(inc x) (inc y)]))
       (recur [(inc x) (inc y)])
 
       ; Settle
       :else [x y])))
 
-(defn simulate-particles [stop? bottom grid]
-  (loop [grid grid]
+(defn simulate-particles [stop? bottom settled]
+  (loop [grid settled]
     (let [particle (simulate-particle bottom grid)]
       (if (stop? particle)
         grid
         (recur (conj grid particle))))))
 
-(def grid
-  (->> parsed
-       (mapcat expand)
-       set))
-
 ; Part 1
-(let [abyss (apply max (map first grid))
+(let [abyss (apply max (map first rocks))
       stop? #(>= (second %) abyss)]
-  (-> (simulate-particles stop? abyss grid)
-      (set/difference grid)
+  (-> (simulate-particles stop? abyss rocks)
+      (set/difference rocks)
       count))
 
 ; Part 2  
-(let [floor (inc (apply max (map second grid)))
+(let [floor (inc (apply max (map second rocks)))
       stop? #(= % [500 0])]
-  (-> (simulate-particles stop? floor grid)
-      (set/difference grid)
+  (-> (simulate-particles stop? floor rocks)
+      (set/difference rocks)
       count
       inc)) ; Include the particle that settled at the top
