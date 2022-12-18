69c6661ffcdf378f4c65dd5fbd7d74c6ff6a94af
volesen <vincolesen@gmail.com>
2022-12-18 15:12:02 +0100

    Cleanup and add explanation

--- day16/day16.clj
+++ day16/day16.clj
@@ -2,80 +2,71 @@
 
 (def input (slurp "day16/input.txt"))
 
-
 (def valves
-  (into
+  (reduce
+   (fn [valves line]
+     (let [[_ valve flow tunnels] (re-matches #"Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)" line)]
+       (assoc valves valve {:flow (parse-long flow)
+                            :tunnels (str/split tunnels #", ")})))
    {}
-   (for [line (str/split-lines input)
-         :let [[_ valve flow tunnels] (re-matches #"Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? (.*)" line)]]
-     [valve {:flow (parse-long flow)
-             :tunnels (str/split tunnels #", ")}])))
+   (str/split-lines input)))
 
-; Calculate pairwise distances
-(def distances
-  (reduce-kv
-   (fn [distances valve {:keys [tunnels]}]
-     (-> distances
-         (assoc [valve valve] 0)
-         (into (for [tunnel tunnels] [[valve tunnel] 1]))))
-   {}
-   valves))
+(defn init-distances [valves]
+  (let [selfs (for [[valve _] valves]
+                [[valve valve] 0])
+        neighbours (for [[valve info] valves
+                         tunnel (:tunnels info)]
+                     [[valve tunnel] 1])]
+    (into {} (concat selfs neighbours))))
 
-;Floyd–Warshall algorithm
-(def distances
-  (let [kij (for [k (keys valves)
-                  i (keys valves)
-                  j (keys valves)]
-              [k i j])]
-    (reduce
-     (fn [distances [k i j]]
-       (assoc distances [i j]
-              (min (get distances [i j] ##Inf)
-                   (+ (get distances [i k] ##Inf)
-                      (get distances [k j] ##Inf)))))
-     distances
-     kij)))
+(defn pairwise-shortest-distance
+  "Floyd–Warshall algorithm"
+  [valves]
+  (reduce
+   (fn [distances [k i j]]
+     (assoc distances [i j]
+            (min (get distances [i j] ##Inf)
+                 (+ (get distances [i k] ##Inf)
+                    (get distances [k j] ##Inf)))))
+   (init-distances valves)
+   (for [k (keys valves)
+         i (keys valves)
+         j (keys valves)]
+     [k i j])))
+
+(def distances (pairwise-shortest-distance valves))
 
 (defn max* [coll]
-  (when (seq coll)
-    (apply max coll)))
+  (if (seq coll)
+    (apply max coll)
+    0))
 
-(def non-zero-valves (filter (fn [[_ v]] (pos? (:flow v))) valves))
-
-(defn max-flow* [t opened-valves valve]
+; Without the elephant helping, this is a classic knapsack problem.
+; With the elephant, we optimize at each t the optimal remaining valves to open (we can stop early),
+; taking into conseration that the elephant can optimally open the remaining set of valves starting,
+; with 26 minutes left.
+(defn max-flow* [t not-opened valve elephant?]
   (let [flow (get-in valves [valve :flow])
-        relieved-pressure (* flow t)
-        next-valves (for [next-valve (keys non-zero-valves)
-                          :let [distance (get distances [valve next-valve])]
-                          :when (and (> t distance) (not (contains? opened-valves next-valve)))]
-                      (max-flow* (- t distance 1) (conj opened-valves next-valve) next-valve))]
-    (+ relieved-pressure
-       (or (max* next-valves) 0))))
-
-(defn max-flow [& args]
-  (with-redefs [max-flow* (memoize max-flow*)]
-    (apply max-flow* args)))
-
-(time (max-flow 30 #{} "AA"))
-
-; Part 2
-
-(defn max-flow*
-  [t not-opened valve elephant?]
-  (let [flow (get-in valves [valve :flow])
-        relieved-pressure (* flow t)
-        next-valves (for [next-valve not-opened
+        next-valves* (for [next-valve not-opened
                           :let [distance (distances [valve next-valve])]
                           :when (> t distance)]
                       (max-flow* (- t distance 1) (disj not-opened next-valve) next-valve elephant?))
-        with-elephant (if elephant? (conj next-valves (max-flow* 26 not-opened "AA" false)) next-valves)]
-    (+ relieved-pressure
-       (or (max* with-elephant) 0))))
+        next-valves (if elephant?
+                        (conj next-valves* (max-flow* 26 not-opened "AA" false))
+                        next-valves*)]
+    (+ (* flow t) (max* next-valves))))
 
+(defn max-flow [t not-opened valve elephant?]
+  (with-redefs [max-flow* (memoize max-flow*)]
+    (max-flow* t not-opened valve elephant?)))
 
 (def non-zero-valves (->> valves
                           (filter (fn [[_ v]] (pos? (:flow v))))
                           keys
                           set))
 
-(time (println (max-flow 26 non-zero-valves "AA" true)))
+; Part 1
+(max-flow 30 non-zero-valves "AA" false)
+
+; Part 2
+(max-flow 26 non-zero-valves "AA" true)
